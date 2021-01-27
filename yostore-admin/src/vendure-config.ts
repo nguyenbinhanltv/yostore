98fa4442b567917fdd9855abecc01e962d329e8f
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2021-01-28 04:09:02 +0700

    build image docker yostore-admin and push to gcp

--- yostore-admin/src/vendure-config.ts
+++ yostore-admin/src/vendure-config.ts
@@ -1,71 +1,75 @@
 import {
-    examplePaymentHandler,
-    DefaultJobQueuePlugin,
-    DefaultSearchPlugin,
-    VendureConfig,
-} from '@vendure/core'; 
-import { defaultEmailHandlers, EmailPlugin } from '@vendure/email-plugin';
-import { AssetServerPlugin } from '@vendure/asset-server-plugin';
-import { AdminUiPlugin } from '@vendure/admin-ui-plugin';
-import path from 'path';
+  examplePaymentHandler,
+  DefaultJobQueuePlugin,
+  DefaultSearchPlugin,
+  VendureConfig,
+} from "@vendure/core";
+import { defaultEmailHandlers, EmailPlugin } from "@vendure/email-plugin";
+import { AssetServerPlugin } from "@vendure/asset-server-plugin";
+import { AdminUiPlugin } from "@vendure/admin-ui-plugin";
+import path from "path";
+import { GoogleStorageStrategy } from "vendure-plugin-google-storage-assets";
 
 export const config: VendureConfig = {
-    apiOptions: {
-        port: 3000,
-        adminApiPath: 'admin-api',
-        adminApiPlayground: {
-            settings: {
-                'request.credentials': 'include',
-            } as any,
-        },// turn this off for production
-        adminApiDebug: true, // turn this off for production
-        shopApiPath: 'shop-api',
-        shopApiPlayground: {
-            settings: {
-                'request.credentials': 'include',
-            } as any,
-        },// turn this off for production
-        shopApiDebug: true,// turn this off for production
+  apiOptions: {
+    port: ((process.env.PORT as unknown) as number) || 3000,
+    adminApiPath: "admin-api",
+    adminApiPlayground: {}, // turn this off for production
+    adminApiDebug: false, // turn this off for production
+    shopApiPath: "shop-api",
+    shopApiPlayground: {}, // turn this off for production
+    shopApiDebug: false, // turn this off for production
+  },
+  authOptions: {
+    superadminCredentials: {
+      identifier: "yostoreadmin",
+      password: process.env.YOSTORE_PASS as string,
     },
-    authOptions: {
-        superadminCredentials: {
-            identifier: 'superadmin',
-            password: 'superadmin',
-        },
-    },
-    dbConnectionOptions: {
-        type: 'better-sqlite3',
-        synchronize: false, // not working with SQLite/SQL.js, see https://github.com/typeorm/typeorm/issues/2576
-        logging: false,
-        database: path.join(__dirname, '../vendure.sqlite'),
-        migrations: [path.join(__dirname, '../migrations/*.ts')],
-    },
-    paymentOptions: {
-        paymentMethodHandlers: [examplePaymentHandler],
-    },
-    customFields: {},
-    plugins: [
-        AssetServerPlugin.init({
-            route: 'assets',
-            assetUploadDir: path.join(__dirname, '../static/assets'),
-            port: 3001,
-        }),
-        DefaultJobQueuePlugin,
-        DefaultSearchPlugin,
-        EmailPlugin.init({
-            devMode: true,
-            outputPath: path.join(__dirname, '../static/email/test-emails'),
-            mailboxPort: 3003,
-            handlers: defaultEmailHandlers,
-            templatePath: path.join(__dirname, '../static/email/templates'),
-            globalTemplateVars: {
-                // The following variables will change depending on your storefront implementation
-                fromAddress: '"example" <noreply@example.com>',
-                verifyEmailAddressUrl: 'http://localhost:8080/verify',
-                passwordResetUrl: 'http://localhost:8080/password-reset',
-                changeEmailAddressUrl: 'http://localhost:8080/verify-email-address-change'
-            },
-        }),
-        AdminUiPlugin.init({ port: 3002 }),
-    ],
+  },
+  dbConnectionOptions: {
+    type: "postgres",
+    synchronize: false, // turn this off for production
+    logging: false,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE_NAME,
+    migrations: [path.join(__dirname, "../migrations/*.ts")],
+  },
+  paymentOptions: {
+    paymentMethodHandlers: [examplePaymentHandler],
+  },
+  customFields: {},
+  plugins: [
+    AssetServerPlugin.init({
+      storageStrategyFactory: () =>
+        new GoogleStorageStrategy("yo-store"),
+      route: "assets",
+      assetUploadDir: path.join(__dirname, "../static/assets"),
+      port: 3001,
+    }),
+    DefaultJobQueuePlugin,
+    DefaultSearchPlugin,
+    EmailPlugin.init({
+      devMode: true,
+      outputPath: path.join(__dirname, "../static/email/test-emails"),
+      mailboxPort: 3003,
+      handlers: defaultEmailHandlers,
+      templatePath: path.join(__dirname, "../static/email/templates"),
+      globalTemplateVars: {
+        // The following variables will change depending on your storefront implementation
+        fromAddress: '"example" <noreply@example.com>',
+        verifyEmailAddressUrl: "http://localhost:8080/verify",
+        passwordResetUrl: "http://localhost:8080/password-reset",
+        changeEmailAddressUrl:
+          "http://localhost:8080/verify-email-address-change",
+      },
+    }),
+    AdminUiPlugin.init({
+      port: 3002,
+      app: {
+        path: path.join(__dirname, "dist/__admin-ui"),
+      },
+    }),
+  ],
 };
